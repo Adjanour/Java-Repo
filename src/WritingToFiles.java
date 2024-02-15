3b009335373ec847ad24735199c5a5ea48866917
Adjanour <adjanour@icloud.com>
2024-02-15 08:52:10 +0000

    init -4

---
+++ src/WritingToFiles.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
+public class WritingToFiles {
+    public static void main(String[] args) throws FileNotFoundException {
+//        File outputFile = new File("output.txt");
+        File inputFile = new File("text.txt");
+
+        Scanner input = new Scanner(inputFile);
+        //creates the file if not exists
+        PrintWriter output = new PrintWriter("output.txt");
+
+        while (input.hasNextLine()){
+            output.println(input.nextLine().toUpperCase());
+        }
+
+        input.close();
+        output.close();
+    }
+}
